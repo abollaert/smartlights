585514b620f296b3903520d8b52329084d208f0d
Alexander Bollaert <alexander.bollaert@gmail.com>
2011-02-07 20:28:56 +0100

    Organize imports.

--- tcp-server/src/main/java/be/abollaert/domotics/light/servers/tcp/api/RemoveMoodHandler.java
+++ tcp-server/src/main/java/be/abollaert/domotics/light/servers/tcp/api/RemoveMoodHandler.java
@@ -1,7 +1,6 @@
 package be.abollaert.domotics.light.servers.tcp.api;
 
 import java.io.IOException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import be.abollaert.domotics.light.api.Mood;
