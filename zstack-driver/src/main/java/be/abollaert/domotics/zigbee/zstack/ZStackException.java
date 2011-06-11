584daf7dd1ebbb027dbdb8c29230958cbffd15bc
Alexander Bollaert <alexander.bollaert@gmail.com>
2011-06-11 17:41:30 +0200

    Serial version UID.

--- zstack-driver/src/main/java/be/abollaert/domotics/zigbee/zstack/ZStackException.java
+++ zstack-driver/src/main/java/be/abollaert/domotics/zigbee/zstack/ZStackException.java
@@ -2,6 +2,11 @@ package be.abollaert.domotics.zigbee.zstack;
 
 public final class ZStackException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	ZStackException(final String message, final Throwable cause) {
 		super(message, cause);
 	}
